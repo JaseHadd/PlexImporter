b4ab35784969398e71ffaa56f4ebd8bea2dd2f66
Jason Frost <jasef@users.noreply.github.com>
2017-05-24 21:06:24 +1000

    Added file processing up until actual moving - printing debug strings.

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,5 +1,17 @@
 import Foundation
 
+extension String {
+    func range(from nsRange : NSRange) -> Range<String.Index>? {
+        let from16 = utf16.startIndex.advanced(by: nsRange.location)
+        let to16 = from16.advanced(by: nsRange.length)
+        if let from = String.Index(from16, within: self),
+            let to = String.Index(to16, within: self) {
+            return from ..< to
+        }
+        return nil
+    }
+}
+
 func runCommand(cmd : String, args : String...) -> (output: [String], error: [String], exitCode: Int32) {
     
     var output : [String] = []
@@ -34,6 +46,49 @@ func runCommand(cmd : String, args : String...) -> (output: [String], error: [St
     return (output, error, status)
 }
 
+func getDirectoryURL(path: String, relativeTo url: URL) -> URL {
+    var newURL = url
+    path.components(separatedBy: "/").forEach { newURL.appendPathComponent($0) }
+    
+    if !FileManager.default.fileExists(atPath: newURL.path) {
+        try! FileManager.default.createDirectory(at: newURL, withIntermediateDirectories: true, attributes: nil)
+    }
+    
+    return newURL
+}
+
+func processFile(withURL url: URL) {
+    let videoExtensions = [".webm", ".mkv", ".flv", ".vog", ".ogv", ".avi", ".mov", ".qt", ".wmv", ".yuv", ".rm", ".rmvb", ".amv", ".mp4", ".m4p", ".m4v", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".m2v", ".m4v", ".3gpp", ".3gpp2", ".flv", ".f4v", ".f4p", ".f4a", ".f4b"]
+    let targetDirectory = URL(fileURLWithPath: "/var/lib/plexmediaserver/", isDirectory: true)
+    
+    if videoExtensions.contains(url.pathExtension) {
+        let stringsToDelete = ["US", "UK"]
+        
+        let fileManager = FileManager.default
+        let fileName = url.lastPathComponent
+        let regex = try! NSRegularExpression(pattern: "([^/]*)[. ][Ss](\\d{1,2})[Ee](\\d{1,2}).*\\.([\\w\\d]{3,4})")
+        let results = regex.matches(in: fileName, options: .init(rawValue: 0), range: NSMakeRange(0, fileName.utf16.count))
+        
+        var showName = fileName.substring(with: fileName.range(from: results[0].rangeAt(1))!)
+        let seasonNumber = fileName.substring(with: fileName.range(from: results[0].rangeAt(2))!)
+        let episodeNumber = fileName.substring(with: fileName.range(from: results[0].rangeAt(3))!)
+        
+        showName = showName.replacingOccurrences(of: ".", with: " ", options: .init(rawValue: 0), range: nil)
+        showName = showName.replacingOccurrences(of: "\\d{4}", with: "($1)", options: .regularExpression, range: nil)
+        
+        let targetPath = "\(showName)/Season \(seasonNumber)"
+        let targetFile = "\(showName) - s\(seasonNumber)e\(episodeNumber).\(url.pathExtension)"
+        
+        let seasonDirectory = getDirectoryURL(path: targetPath, relativeTo: targetDirectory)
+        
+        print("Candidate:", seasonDirectory.path, targetFile, separator: " ")
+        
+        
+        
+//        ([^/]*)[. ][Ss](\d{1,2})[Ee](\d{1,2}).*\.([\w\d]{3,4})
+    }
+}
+
 let basePath = "/var/lib/transmission-daemon/downloads/"
 let test = runCommand(cmd: "/usr/bin/transmission-remote", args: "-l");
 for outputLine in test.output {
@@ -42,17 +97,17 @@ for outputLine in test.output {
     
     guard let id = components.first, let name = components.last else { continue }
     
-    let fileManager = FileManager()
+    let fileManager = FileManager.default
     let torrentURL = URL(fileURLWithPath: basePath.appending(name))
-        
+    
     if let files = try? fileManager.contentsOfDirectory(at: torrentURL, includingPropertiesForKeys: nil, options: .skipsSubdirectoryDescendants) {
-        print("Found a directory, with contents ", files);
+        files.forEach { file in processFile(withURL: file) }
     }
     else if(fileManager.fileExists(atPath: torrentURL.path)) {
-        print("Found a file, ", torrentURL.path)
+        processFile(withURL: torrentURL)
     }
     else {
-        print(basePath.appending(name), "doesn't exist", separator: " ")
+        print("Error processing", name, ": couldn't find file.")
     }
     
     // ugh. if file exists ... if file, else if directory ... also have to update other script because it's adding them every hour no matter what.
